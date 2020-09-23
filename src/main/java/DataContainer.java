38ed1867b425c77102e5e3f8a2692cb47756707a
ilnur <ilnur.nasybullin2000@yandex.ru>
2020-09-23 23:06:13 +0300

    add XML

---
+++ src/main/java/DataContainer.java
@@ -0,0 +1,38 @@
+import airlines.db.Airport;
+import airlines.db.Flight;
+import airlines.db.Route;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataContainer {
+    public static List<Flight> getFlights(int count) {
+        List<Flight> flights = new ArrayList<>(count);
+        Route route = getRoute();
+
+        for (int i = 0; i < count; i++) {
+            LocalDateTime start = getRandomDate();
+            LocalDateTime end = start.plusHours(7);
+
+            flights.add(new Flight(route, start, end));
+        }
+
+        return flights;
+    }
+
+    private static LocalDateTime getRandomDate() {
+        int day = (int) (Math.random() * 30 + 1);
+        int hour = (int) (Math.random() * 5 + 8);
+        return LocalDateTime.of(2020, 8, day, hour, 0, 0);
+    }
+
+    private static Route getRoute() {
+        Airport from = new Airport("UL56");
+        Airport to = new Airport("RU01");
+
+        Route route = new Route(from, to);
+
+        return route;
+    }
+}
---
+++ src/main/java/XMLExample.java
@@ -0,0 +1,28 @@
+import airlines.db.Flight;
+import com.thoughtworks.xstream.XStream;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.Comparator;
+import java.util.List;
+
+public class XMLExample {
+    public static void main(String[] args) throws IOException {
+        List<Flight> flightList = DataContainer.getFlights(10);
+        File file = new File("./src/main/resources/flights.xml");
+
+        XStream xStream = new XStream();
+        xStream.alias("flights", Flight.class);
+        String inputXML = xStream.toXML(flightList);
+
+        Files.write(file.toPath(), inputXML.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+
+        String outputXML = Files.readString(file.toPath());
+        List<Flight> list = (List<Flight>) xStream.fromXML(outputXML);
+
+        list.sort(Comparator.comparing(Flight::getStart));
+        list.forEach(System.out::println);
+    }
+}
